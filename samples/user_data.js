05ec2ec885243119b45ee66a0d427900956b2434
Philip Dow <phil@phildow.net>
2014-01-29 18:53:04 -0600

    added samples

---
+++ samples/user_data.js
@@ -0,0 +1,32 @@
+var users = [
+  {
+    name: "Philip Dow",
+    screenname: "pdow",
+    id: 0
+  },
+  {
+    name: "Marky Mark",
+    screenname: "marky",
+    id: 1
+  },
+  {
+    name: "Clark Kent",
+    screenname: "superman",
+    id: 2
+  },
+  {
+    name: "Mr. T",
+    screenname: "pitythefool",
+    id: 3
+  },
+  {
+    name: "Barack Obama",
+    screenname: "potus",
+    id: 4
+  },
+  {
+    name: "Dolly Parton",
+    screenname: "stillalive",
+    id: 5
+  }
+];
